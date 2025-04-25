f36a30139c19adc3482f32836e9a51242172241e
booobi <qqwweee59@gmail.com>
2025-04-25 13:19:56 +0300

    add ngsw

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,11 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideHttpClient } from '@angular/common/http';
 import { register } from 'swiper/element/bundle';
+import { provideServiceWorker } from '@angular/service-worker';
 
 // TODO: in app initializer
 // register Swiper custom elements
@@ -15,6 +16,9 @@ export const appConfig: ApplicationConfig = {
     provideAnimationsAsync(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient(),
+    provideHttpClient(), provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+          }),
   ],
 };
